622a581febf945b38ec85444672dffbc12206c42
ZippyStew45 <38433129+ZippyStew45@users.noreply.github.com>
2023-03-25 23:34:58 -0400

    Add files via upload

---
+++ Game Launcher/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Game_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.Minimize = new System.Windows.Forms.Button();
+            this.AppHeader = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.Gray;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.Black;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(11, 79);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(323, 224);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(11, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(323, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open Application";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Location = new System.Drawing.Point(299, 12);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(35, 21);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "X";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Minimize
+            // 
+            this.Minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Minimize.BackColor = System.Drawing.Color.Olive;
+            this.Minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Minimize.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Minimize.Location = new System.Drawing.Point(258, 12);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Size = new System.Drawing.Size(35, 21);
+            this.Minimize.TabIndex = 3;
+            this.Minimize.Text = "-";
+            this.Minimize.UseVisualStyleBackColor = false;
+            this.Minimize.Click += new System.EventHandler(this.Minimize_Click);
+            // 
+            // AppHeader
+            // 
+            this.AppHeader.AutoSize = true;
+            this.AppHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AppHeader.ForeColor = System.Drawing.Color.Red;
+            this.AppHeader.Location = new System.Drawing.Point(13, 12);
+            this.AppHeader.Name = "AppHeader";
+            this.AppHeader.Size = new System.Drawing.Size(152, 16);
+            this.AppHeader.TabIndex = 4;
+            this.AppHeader.Text = "Application Launcher";
+            this.AppHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.HeaderLabelMouseDown);
+            this.AppHeader.MouseMove += new System.Windows.Forms.MouseEventHandler(this.HeaderLabelMouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(346, 327);
+            this.Controls.Add(this.AppHeader);
+            this.Controls.Add(this.Minimize);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(346, 327);
+            this.Name = "Form1";
+            this.Text = "Application Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormMouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button Minimize;
+        private System.Windows.Forms.Label AppHeader;
+    }
+}
+
---
+++ Game Launcher/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace Game_Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Point MouseLocation;
+        string item = "";
+        public Form1()
+        {
+            InitializeComponent();
+            string[] array2 = Directory.GetFiles(Environment.CurrentDirectory + "\\Aplications\\", "*.lnk");
+            foreach (string name in array2)
+            {
+
+                string[] options = name.Split(new string[] { "\\" }, StringSplitOptions.RemoveEmptyEntries);
+                string filename = options.Last().Replace(".lnk", "");
+
+                listBox1.Items.Add(filename);
+                if (listBox1.SelectedItem != null)
+                item = listBox1.GetItemText(listBox1.SelectedItem); 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var selecteditem in listBox1.SelectedItems)
+            {
+                Process.Start(Environment.CurrentDirectory + "\\Aplications\\" + selecteditem);
+            }
+        }
+
+        private void FormMouseDown(object sender, MouseEventArgs e)
+        {
+            MouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void FormMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point MousePosition = Control.MousePosition;
+                MousePosition.Offset(MouseLocation.X, MouseLocation.Y);
+                Location = MousePosition;
+            }
+        }
+
+        private void HeaderLabelMouseDown(object sender, MouseEventArgs e)
+        {
+            MouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void HeaderLabelMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point MousePosition = Control.MousePosition;
+                MousePosition.Offset(MouseLocation.X, MouseLocation.Y);
+                Location = MousePosition;
+            }
+        }
+
+        private void Minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
